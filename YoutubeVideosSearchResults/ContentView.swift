22e7fdd4813d228f119aff812ca02684ed3386a7
iMokhles <mokhleshussien@aol.com>
2021-11-24 17:00:37 +0200

    Initial Commit

---
+++ YoutubeVideosSearchResults/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YoutubeVideosSearchResults
+//
+//  Created by iMokhles on 24/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
